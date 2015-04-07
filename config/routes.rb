97ad30bf3f2f97bcc15d379ef783e36cba1acbf7
Narbe Voskanian <narbevoskanian@gmail.com>
2015-04-07 11:26:50 -0700

    add users and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :static_pages, only: [:index], path: ''
+  resources :users, only: [:create, :new]
+  resource :session, only: [:new, :create, :destroy]
 end
