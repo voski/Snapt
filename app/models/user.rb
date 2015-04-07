f8497286eecf79d74cc78f13ccd2e4763c6c3681
Narbe Voskanian <narbevoskanian@gmail.com>
2015-04-07 11:58:06 -0700

    auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   attr_reader :password
 
-  def Self.find_by_credentials(username, password)
+  def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user && user.password?(password)
     user
