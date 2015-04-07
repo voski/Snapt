011e930b58eaf7971b992cae47d299ff734d075a
Narbe Voskanian <narbevoskanian@gmail.com>
2015-04-07 15:38:02 -0700

    fix partial ...again, sign in up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,21 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    
+    if @user
+      sign_in(@user)
+      redirect_to static_pages_url
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to new_session_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
       sign_in(@user)
       redirect_to static_pages_url
     else
-      flash.errors = @user.errors.full_messages
-      render new
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
     end
   end
 
