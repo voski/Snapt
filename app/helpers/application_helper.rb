212be29c8da77a8560c387969702231a498ebc5b
Narbe Voskanian <narbevoskanian@gmail.com>
2015-04-07 15:48:43 -0700

    sign out button with helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def auth_token
     "<input type='hidden' name='authenticity_token' value='#{ form_authenticity_token }'>".html_safe
   end
+
+  def sign_out_btn
+    button_to 'Sign Out', session_url, method: :delete
+  end
 end
