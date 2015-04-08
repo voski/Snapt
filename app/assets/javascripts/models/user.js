2e89675b7a8d27dcfe33816c7ae4fd1110f88183
Narbe Voskanian <narbevoskanian@gmail.com>
2015-04-08 13:44:59 -0700

    add BB user model with photos assoc

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,4 +1,21 @@
 Snapt.Models.User = Backbone.Model.extend({
-  urlRoot: 'api/users'
+  urlRoot: '/api/users',
+
+  parse: function (payload) {
+    if (payload.photos) {
+      this.photos.set(payload.photos, { parse: true })
+      delete payload.photos
+    }
+
+    return payload;
+  },
+
+  photos: function () {
+    if (!this._photos) {
+      this.photos = new Snapt.Collections.Photos([], author: this)
+    }
+
+    return this._photos
+  },
 
 })
