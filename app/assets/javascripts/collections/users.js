df07b35b9751d69f5b4404ad25fcdb802fe1ba14
Narbe Voskanian <narbevoskanian@gmail.com>
2015-04-09 13:48:44 -0700

    getOrFetch

--- app/assets/javascripts/collections/users.js
+++ app/assets/javascripts/collections/users.js
@@ -1,6 +1,24 @@
 Snapt.Collections.Users = Backbone.Collection.extend({
   url: '/api/users',
   model: Snapt.Models.User,
+
+  getOrFetch: function (id) {
+    collection = this;
+
+    var model = this.get(id);
+    if (model) {
+      model.fetch()
+    } else {
+      model = new Snapt.Models.User({{ id: id }})
+      model.fetch({
+        success: function () {
+          collection.add(model)
+        }
+      });
+    }
+
+    return model;
+  }
 });
 
 Snapt.Collections.users = new Snapt.Collections.Users();
