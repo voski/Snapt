f7dc13d0419288178d33c165e9408370d02e469e
Narbe Voskanian <narbevoskanian@gmail.com>
2015-04-09 18:30:51 -0700

    crop photos, photo url helper method

--- app/assets/javascripts/models/photo.js
+++ app/assets/javascripts/models/photo.js
@@ -7,5 +7,15 @@ Snapt.Models.Photo = Backbone.Model.extend({
 
   isAuthor: function() {
     return Snapt.currentUser.id === this.get('author_id');
+  },
+
+  cloudinaryUrl: function(options) {
+    return (
+      $.cloudinary.image(
+        this.escape('public_id'),
+        { width: 510, height: 510, crop: 'fill' }
+      )[0].outerHTML
+    )
+
   }
 });
