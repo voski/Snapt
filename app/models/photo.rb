2afe924afdd82031cddbc606019010c0d740dc4a
Narbe Voskanian <narbevoskanian@gmail.com>
2015-04-14 16:29:11 -0700

    comments destroyed with photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -14,7 +14,7 @@ class Photo < ActiveRecord::Base
   validates :author_id, :url, :public_id, presence: true
 
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   default_scope { order(:created_at) }
 end
