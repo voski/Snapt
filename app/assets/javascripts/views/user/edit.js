91d27abfe80685a51dfb54b92594db552ce423fd
Narbe Voskanian <narbevoskanian@gmail.com>
2015-04-17 13:37:33 -0700

    rendering edit page, some styling

--- app/assets/javascripts/views/user/edit.js
+++ app/assets/javascripts/views/user/edit.js
@@ -1,6 +1,12 @@
 Snapt.Views.EditUser = Backbone.CompositeView.extend({
   template: JST['user/edit'],
+
   className: 'container user-show jumbotron',
 
+  render: function () {
+    var content = this.template()
+    this.$el.html(content);
 
+    return this;
+  }
 });
